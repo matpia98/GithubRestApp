a6a69f38c128bbfe5e5f1cbfa26ada76d3f16059
Mateusz <matpia98@gmail.com>
2024-07-21 01:04:17 +0200

    Initial commit

---
+++ src/main/java/com/example/githubrestapp/GithubRestAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubrestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubRestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/githubrestapp/GithubRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubrestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
